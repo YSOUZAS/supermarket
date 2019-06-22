dbda3ca6220a5bb0f49094e050b070a175ea2d81
ysouzas <6200092+YSOUZAS@users.noreply.github.com>
2019-06-22 09:28:04 +0100

    update splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      checkerboardRasterCacheImages: true,
       locale: Locale('pt-br', 'BR'),
       theme: ThemeData(
         primarySwatch: Colors.blue,
